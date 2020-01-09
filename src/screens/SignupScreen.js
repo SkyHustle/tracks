e2c4bae45fe20ee326855e2fbfe256857f5af4a1
Dmitry Sky <dmitry.sky@icloud.com>
2020-01-09 13:11:30 -1000

    email and password should be a part of the signup components state and both should not be autocapitalized or autocorrected

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Text, Input, Button } from 'react-native-elements';
 import Spacer from '../components/Spacer';
 
 const SignupScreen = ({ navigation }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return(
     <View style={styles.container}>
       <Spacer>
         <Text h3>Sign Up</Text>
       </Spacer>
       <Spacer>
-        <Input label="Email" />
+        <Input
+          autoCapitalize="none"
+          autoCorrect={false}
+          label="Email"
+          value={email}
+          onChangeText={(newEmail) => setEmail(newEmail)}
+        />
       </Spacer>
       <Spacer>
-        <Input label="Password" />
+        <Input
+          autoCapitalize="none"
+          autoCorrect={false}
+          label="Password"
+          value={password}
+          onChangeText={(newPassword) => setPassword(newPassword)}
+        />
       </Spacer>
       <Spacer>
         <Button title="Sign Up" />
