dd0604b2c13091136283a417d433356ee7250c63
Dmitry Sky <dmitry.sky@icloud.com>
2020-01-09 12:00:05 -1000

    user should be able to navigate from Signup to Signin screen

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, Button } from 'react-native';
 
-const SignupScreen = () => {
-  return <Text style={{ fontSize: 48 }}>SignupScreen</Text>;
+const SignupScreen = ({ navigation }) => {
+  return(
+    <View>
+      <Text style={{ fontSize: 48 }}>SignupScreen</Text>
+      <Button title='Signin' onPress={() => navigation.navigate('Signin')}/>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({});
