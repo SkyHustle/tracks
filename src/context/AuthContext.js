e75258ae8fa484d93196cfcc67ee38099a7cbe73
Dmitry Sky <dmitry.sky@icloud.com>
2020-01-12 09:25:37 -1000

    user should receive a jwt after successfully signing up

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,3 +1,4 @@
+import { AsyncStorage } from 'react-native';
 import createDataContext from './createDataContext';
 import trackerApi from '../api/tracker';
 
@@ -6,6 +7,8 @@ const authReducer = (state, action) => {
   switch (action.type) {
     case 'add_error':
       return { ...state, errorMessage: action.payload };
+    case 'signup':
+      return { errorMessage: '', token: action.payload };
     default:
       return state;
   }
@@ -15,7 +18,8 @@ const signup = (dispatch) => {
   return async ({ email, password }) => {
     try {
       const response = await trackerApi.post('/signup', { email, password });
-      console.log(response.data);
+      await AsyncStorage.setItem('token', response.data.token);
+      dispatch({ type: 'signup', payload: response.data.token })
     } catch (err) {
       // We always call dispatch every time we want to update our state
       dispatch({ type: 'add_error', payload: 'Something went wrong with sign up' })
@@ -39,5 +43,5 @@ const signout = (dispatch) => {
 export const { Provider, Context } = createDataContext(
   authReducer,
   {signin, signout, signup},
-  { isSignedIn: false, errorMessage: '' }
+  { token: null, errorMessage: '' }
 );
