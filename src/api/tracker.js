179b2b473cedf49d566b2eee118aa64c1e5199de
Dmitry Sky <dmitry.sky@icloud.com>
2020-01-13 14:58:06 -1000

    create reusable component with some boilerplate for our authentication stuff.

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 export default axios.create({
   // This Temp URL provided by ngrok only works for 8 Hours
-  baseURL: 'http://8dfeb7eb.ngrok.io'
+  baseURL: 'http://96b1b9cb.ngrok.io'
 });
 
