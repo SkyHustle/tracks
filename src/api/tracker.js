12634e5e81f6e166d92b08b62043fbc84f015eeb
Dmitry Sky <dmitry.sky@icloud.com>
2020-01-15 19:19:03 -1000

    remove some un-used code

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 export default axios.create({
   // This Temp URL provided by ngrok only works for 8 Hours
-  baseURL: 'http://96b1b9cb.ngrok.io'
+  baseURL: 'http://7ccf5558.ngrok.io'
 });
 
