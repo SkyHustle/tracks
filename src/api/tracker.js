cb7b6c9d79b13e5a25e83d22f36ccb6cc1ee2c89
Dmitry Sky <dmitry.sky@icloud.com>
2020-01-16 11:50:57 -1000

    existing user should remain signed in when she leaves the app and comes back.

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 export default axios.create({
   // This Temp URL provided by ngrok only works for 8 Hours
-  baseURL: 'http://7ccf5558.ngrok.io'
+  baseURL: 'http://fa9c96bb.ngrok.io'
 });
 
