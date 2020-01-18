a914dd2a7fae46a498975f6d142de6d956d4ed72
Dmitry Sky <dmitry.sky@icloud.com>
2020-01-18 11:46:57 -1000

    install expo location service

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { Text, StyleSheet } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, { Polyline } from 'react-native-maps';
 
 const Map = () => {
-  return <MapView
-    style={styles.map}
-    initialRegion={{
-      latitude: 21.649902,
-      longitude: -158.062805,
-      latitudeDelta: 0.01,
-      longitudeDelta: 0.01
-    }}
-  />;
+  return(
+    <MapView
+      style={styles.map}
+      initialRegion={{
+        latitude: 21.649902,
+        longitude: -158.062805,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01
+      }}
+    >
+      <Polyline coordinates={[]} />
+    </MapView>
+  );
 };
 
 const styles = StyleSheet.create({
