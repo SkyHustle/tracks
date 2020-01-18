f576c02f82712d0bba988b3a5d3910193a59925c
Dmitry Sky <dmitry.sky@icloud.com>
2020-01-18 11:15:04 -1000

    create a Map component with some boilerplate and display on create track screen

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 export default axios.create({
   // This Temp URL provided by ngrok only works for 8 Hours
-  baseURL: 'http://fa9c96bb.ngrok.io'
+  baseURL: 'http://3fde2380.ngrok.io'
 });
 
