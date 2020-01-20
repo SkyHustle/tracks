0edc3af9a1730b01235089ab0b4ba4328c85551a
Dmitry Sky <dmitry.sky@icloud.com>
2020-01-19 19:43:46 -1000

    youz a bad ass mofo

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  // This Temp URL provided by ngrok only works for 8 Hours
+  // This Temp URL provided by ngrok only works for 8 Hours 
   baseURL: 'http://3fde2380.ngrok.io'
 });
 
